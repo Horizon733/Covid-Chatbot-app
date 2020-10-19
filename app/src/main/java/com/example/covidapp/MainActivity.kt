0b53a647be7f6053e53700e2ccc601560b3f5a12
Horizon733 <dishantgandhi733@gmail.com>
2020-10-19 11:52:09 +0530

    First Commit

--- app/src/main/java/com/example/covidapp/MainActivity.kt
+++ app/src/main/java/com/example/covidapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.covidapp
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.NestedScrollView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import okhttp3.OkHttpClient
 import retrofit2.Call
@@ -19,6 +25,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var editText: EditText
     private lateinit var sendBtn: FloatingActionButton
+    private lateinit var recyclerView: RecyclerView
     private val USER = 0
     private val BOT = 1
 
@@ -31,46 +38,56 @@ class MainActivity : AppCompatActivity() {
         editText = findViewById(R.id.edittext_chatbox)
         chatScrollView.post { chatScrollView.fullScroll(ScrollView.FOCUS_DOWN) }
         sendBtn = findViewById(R.id.send_button)
-        sendBtn.setOnClickListener{
-            sendMessage()
+
+        sendBtn.setOnClickListener {
+            val msg: String = editText.text.toString().trim()
+            sendMessage(msg)
         }
 
 
     }
 
-    fun sendMessage(){
-        val msg:String = editText.text.toString().trim()
+    fun sendMessage(message: String) {
         val date = Date(System.currentTimeMillis())
-
-       val okHttpClient = OkHttpClient()
+        val okHttpClient = OkHttpClient()
         val retrofit = Retrofit.Builder()
             .baseUrl("http://192.168.0.105:5005/webhooks/rest/")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val userMessage = UserMessage()
-        if(msg.trim().isEmpty())
-            Toast.makeText(this,"Please enter your query",Toast.LENGTH_SHORT).show()
+        if (message.trim().isEmpty())
+            Toast.makeText(this, "Please enter your query", Toast.LENGTH_SHORT).show()
         else {
-            Log.e("MSg","msssage: $msg")
-            editText.setText("")
-            userMessage.UserMessage("User",msg)
-            showTextView(msg,USER,date.toString())
+            Log.e("MSg", "msssage: $message")
+            userMessage.UserMessage("User", message)
+            showTextView(message, USER, date.toString())
 
         }
-       val messageSender = retrofit.create(MessageSender::class.java)
+        val messageSender = retrofit.create(MessageSender::class.java)
         val response =
             messageSender.sendMessage(userMessage)
 
         response.enqueue(object : Callback<List<BotResponse>> {
-            override fun onResponse(call: Call<List<BotResponse>>, response: Response<List<BotResponse>>) {
+            override fun onResponse(
+                call: Call<List<BotResponse>>,
+                response: Response<List<BotResponse>>
+            ) {
                 if (response.body() == null || response.body()!!.size == 0) {
                     val botMessage = "Sorry didn't understand"
-                    showTextView(botMessage,BOT,date.toString())
+                    showTextView(botMessage, BOT, date.toString())
                 } else {
                     val botResponse = response.body()!![0]
-                    showTextView(botResponse.text,BOT,date.toString())
-                    if(botResponse.buttons != null) {
+                    showTextView(botResponse.text, BOT, date.toString())
+                    if (botResponse.buttons != null) {
+
+                        val buttonRecyclerView =
+                            ButtonRecyclerView(this@MainActivity, botResponse.buttons)
+                        recyclerView = findViewById(R.id.button_list)
+                        recyclerView.adapter = buttonRecyclerView
+                        val layoutManager = LinearLayoutManager(this@MainActivity)
+                        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+                        recyclerView.layoutManager = layoutManager
                         Log.e("Button c", "${botResponse.buttons.size}")
                     }
                 }
@@ -78,22 +95,25 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<List<BotResponse>>, t: Throwable) {
                 val botMessage = "Check your network connection"
-                showTextView(botMessage,BOT,date.toString())
+                showTextView(botMessage, BOT, date.toString())
                 t.printStackTrace()
                 Toast.makeText(this@MainActivity, "" + t.message, Toast.LENGTH_SHORT).show()
             }
         })
 
     }
-    fun showTextView(message:String,type:Int,date:String){
+
+    fun showTextView(message: String, type: Int, date: String) {
         var frameLayout: FrameLayout? = null
         val linearLayout = findViewById<LinearLayout>(R.id.chat_layout)
-        when(type){
-            USER -> { frameLayout = getUserLayout()
+        when (type) {
+            USER -> {
+                frameLayout = getUserLayout()
             }
-            BOT ->{frameLayout = getBotLayout()
+            BOT -> {
+                frameLayout = getBotLayout()
             }
-            else->{
+            else -> {
                 frameLayout = getBotLayout()
             }
         }
@@ -109,13 +129,13 @@ class MainActivity : AppCompatActivity() {
         val currentDate = dateFormat.format(currentDateTime)
         val providedDate = dateFormat.format(dateNew)
         var time = ""
-        if(currentDate.equals(providedDate)) {
+        if (currentDate.equals(providedDate)) {
             val timeFormat = SimpleDateFormat(
                 "hh:mm aa",
                 Locale.ENGLISH
             )
             time = timeFormat.format(dateNew)
-        }else{
+        } else {
             val dateTimeFormat = SimpleDateFormat(
                 "dd-MM-yy hh:mm aa",
                 Locale.ENGLISH
@@ -127,6 +147,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
     fun getUserLayout(): FrameLayout? {
         val inflater: LayoutInflater = LayoutInflater.from(this)
         return inflater.inflate(R.layout.user_message_box, null) as FrameLayout?
@@ -135,5 +156,36 @@ class MainActivity : AppCompatActivity() {
     fun getBotLayout(): FrameLayout? {
         val inflater: LayoutInflater = LayoutInflater.from(this)
         return inflater.inflate(R.layout.bot_message_box, null) as FrameLayout?
+    }
+
+
+    inner class ButtonRecyclerView(var context: Context, var buttons: List<BotResponse.Buttons>) :
+        RecyclerView.Adapter<ButtonRecyclerView.ButtonViewHolder>() {
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ButtonViewHolder {
+            val view =
+                LayoutInflater.from(context).inflate(R.layout.button_list_item, parent, false)
+            return ButtonViewHolder(view)
+
+        }
+
+        override fun onBindViewHolder(holder: ButtonViewHolder, position: Int) {
+            val payload_button = buttons[position]
+            holder.button.text = payload_button.title
+            holder.button.setOnClickListener {
+                sendMessage(payload_button.payload)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            buttons.isEmpty() ?: return -1
+            return buttons.size
+
+        }
+
+        inner class ButtonViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val button = view.findViewById<MaterialButton>(R.id.payload_button)
+        }
     }
 }
